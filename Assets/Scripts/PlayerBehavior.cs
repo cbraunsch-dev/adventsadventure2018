be803e19a984a35524402810af61e105aebcc41e
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-11-08 06:52:30 +0100

    #124 - Create player, board and read keyboard input

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("d")) {
+            Debug.Log("Move to next field");
+        } else if (Input.GetKeyDown("a")) {
+            Debug.Log("Move to previous field");
+        }
+	}
+}

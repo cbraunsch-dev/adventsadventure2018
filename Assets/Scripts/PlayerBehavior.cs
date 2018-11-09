336475bab307ef9c4fc86dd23ce69e8a34d35447
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-11-09 06:28:49 +0100

    #125 - Move player between spaces

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -3,18 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
+    public GameObject startingSpace;
+    public float speed = 5;
+
+    private GameObject currentSpace;
+    private Vector3 targetPosition;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        this.currentSpace = startingSpace;
+        SetPositionAsTarget(this.currentSpace);
+    }
 	
 	// Update is called once per frame
     void Update () {
         if (Input.GetKeyDown("d")) {
-            Debug.Log("Move to next field");
+            MoveToNextSpace(this.currentSpace);
         } else if (Input.GetKeyDown("a")) {
-            Debug.Log("Move to previous field");
+            MoveToPreviousSpace(this.currentSpace);
         }
+
+        //Move towards target
+        MoveTowards(targetPosition);
 	}
+
+    void MoveToNextSpace(GameObject space) {
+        var nextSpace = space.GetComponent<SpaceBehavior>().nextSpace;
+        if (nextSpace != null) {
+            this.currentSpace = nextSpace;
+            SetPositionAsTarget(this.currentSpace);
+        }
+    }
+
+    void MoveToPreviousSpace(GameObject space) {
+        var previousSpace = space.GetComponent<SpaceBehavior>().previousSpace;
+		if (previousSpace != null)
+		{
+			this.currentSpace = previousSpace;
+            SetPositionAsTarget(this.currentSpace);
+		}
+    }
+
+    void SetPositionAsTarget(GameObject space) {
+        //Lock the y-axis so that the player remains at a constant height while moving. The game board will be flat
+        this.targetPosition = new Vector3(space.transform.position.x, this.transform.position.y, space.transform.position.z);
+    }
+
+    void MoveTowards(Vector3 position) {
+		//Move player between game spaces
+		// The step size is equal to speed times frame time.
+		float step = speed * Time.deltaTime;
+
+		// Move our position a step closer to the target
+        transform.position = Vector3.MoveTowards(transform.position, position, step);
+    }
 }
---
+++ Assets/SpaceBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceBehavior : MonoBehaviour {
+    public GameObject previousSpace;
+    public GameObject nextSpace;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

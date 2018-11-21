7ec474a8fc3a3466063a618f85dc9cd8ee06bbc2
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-11-21 06:42:42 +0100

    #177 - Don't destroy the canvas that holds the button to launch the movement mechanic when changing scenes since its listener is the game manager which also doesn't get destroyed when scenes are changed

---
+++ Assets/Scripts/MovementCanvasBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCanvasBehavior : MonoBehaviour {
+
+	private static MovementCanvasBehavior instance = null;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if (instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+	}
+}
